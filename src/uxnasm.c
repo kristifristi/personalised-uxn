202ca788009978a204dbb0d6892eea89315828d5
Devine Lu Linvega <aliceffekt@gmail.com>
2024-02-25 16:06:54 -0800

    (uxnasm) Added support for partial label jsi

--- src/uxnasm.c
+++ src/uxnasm.c
@@ -195,7 +195,7 @@ makereference(char *scope, char *label, char rune, Uint16 addr)
 	if(label[0] == '{') {
 		p.lambda_stack[p.lambda_ptr++] = p.lambda_count;
 		scpy(makelambda(p.lambda_count++), r->name, 0x40);
-	} else if(label[0] == '&') {
+	} else if(label[0] == '&' || label[0] == '/') {
 		if(!sublabel(subw, scope, label + 1))
 			return error("Invalid sublabel", label);
 		scpy(subw, r->name, 0x40);
@@ -502,7 +502,7 @@ main(int argc, char *argv[])
 	if(argc == 1)
 		return error("usage", "uxnasm [-v] input.tal output.rom");
 	if(argv[1][0] == '-' && argv[1][1] == 'v')
-		return !fprintf(stdout, "Uxnasm - Uxntal Assembler, 24 Feb 2024.\n");
+		return !fprintf(stdout, "Uxnasm - Uxntal Assembler, 25 Feb 2024.\n");
 	if(!(src = fopen(argv[1], "r")))
 		return !error("Invalid input", argv[1]);
 	if(!assemble(src))
